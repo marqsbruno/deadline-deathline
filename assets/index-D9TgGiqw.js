3ffe4d5845ad9d0b4032529fd4177043b9348719
marqsbruno <marqsbruno@gmail.com>
2024-06-19 23:58:02 -0300

    Updates

--- assets/index-D9TgGiqw.js
+++ assets/index-D9TgGiqw.js
@@ -97,9 +97,9 @@ Error generating stack: `+o.message+`
   box-shadow: 35px 35px #2d3142;
 
   @media (max-width: 768px) {
-    padding: 0.8rem;
-    width: 70%;
-    box-shadow: 25px 25px #2d3142;
+    padding: 1rem;
+    width: 75%;
+    box-shadow: 30px 39px #2d3142;
   }
 `;function s0(){return z.jsxs(u0,{children:[z.jsx(i0,{}),z.jsx(Ym,{})]})}const a0=Hm`
   *{
